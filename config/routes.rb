3bf01dcc404b5a2f4db11aa75954419932409cc6
Martin K <mkmita@gmail.com>
2017-09-05 01:38:30 +0000

    update pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'pages/about'
 
-  get 'pages/home'
+  root 'pages#home'
+  get 'about' => 'pages#about'
 
-  # The priority is based upon order of creation: first created -> highest priority.
+
+  # The priority is based upon order of creation: first created ->highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
